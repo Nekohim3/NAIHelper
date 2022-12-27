b1cb96b77f3a330b9295987e5f99d658539f399e
Neko <kuroneko@protonmail.ch>
2022-12-27 11:51:29 +0300

    Init

---
+++ NAIHelper/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NAIHelper.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
